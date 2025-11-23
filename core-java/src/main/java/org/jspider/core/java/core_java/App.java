54f8746a56efd35db3b9eab0104f4153497e9c35
akonds <darknightcandle@gmail.com>
2025-11-23 21:09:16 +0100

    primary file upload

---
+++ core-java/src/main/java/org/jspider/core/java/core_java/App.java
@@ -0,0 +1,13 @@
+package org.jspider.core.java.core_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
